5cce5645c19dd66f3b2d1e0522df8a616df21791
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2023-10-19 15:28:39 +0200

    wait for space to be ready

--- scripts/restart_hf_space.py
+++ scripts/restart_hf_space.py
@@ -1,10 +1,22 @@
 from huggingface_hub import HfApi
+import time
 import os
 
-api = HfApi()
 
-# update space
-api.restart_space(
-    repo_id="IlyasMoutawwakil/dana",
-    token=os.environ.get("HF_TOKEN")
-)
+api = HfApi()
+HF_TOKEN = os.environ.get("HF_TOKEN")
+
+# restart space
+api.restart_space(repo_id="IlyasMoutawwakil/dana", token=HF_TOKEN)
+
+# wait for the space to be ready
+while True:
+    runtime = api.get_space_runtime(repo_id="IlyasMoutawwakil/dana", token=HF_TOKEN)
+
+    if runtime.stage in ["RUNNING"]:
+        break
+
+    elif runtime.stage in ["BUILD_ERROR", "RUNTIME_ERROR", "CONFIG_ERROR"]:
+        raise RuntimeError(f"Error in the space: {runtime.stage}")
+
+    time.sleep(5)
