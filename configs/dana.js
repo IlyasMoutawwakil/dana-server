e180d798d8ea5f01509062dcc34b7aaf90e646a1
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2023-11-07 13:25:49 +0100

    moved creating projects to the space dockerfile

--- configs/dana.js
+++ configs/dana.js
@@ -11,18 +11,8 @@ module.exports = {
       password: process.env.ADMIN_PASSWORD || "admin",
       email: process.env.ADMIN_EMAIL || "admin@gmail.com",
     },
-    {
-      username: "amd-user",
-      password: process.env.AMD_USER_PASSWORD || "amd-user",
-      email: process.env.AMD_USER_EMAIL || "amd-user@gmail.com",
-    },
-    {
-      username: "nvidia-user",
-      password: process.env.NVIDIA_USER_PASSWORD || "nvidia-user",
-      email: process.env.NVIDIA_USER_EMAIL || "nvidia-user@gmail.com",
-    },
   ],
   secureCookie: false,
-  apiToken: process.env.API_TOKEN,
+  apiToken: process.env.API_TOKEN || "api-token",
   sessionSecret: crypto.randomBytes(64).toString("hex"),
 };
